6f94f53c5fc7b73eda9449cabd87af383e1ff48f
John Neri <73387545+sugoides@users.noreply.github.com>
2024-02-14 06:18:40 +0800

    update logic again

--- will-you-be-my-valentine/script.js
+++ will-you-be-my-valentine/script.js
@@ -15,13 +15,14 @@ function preloadImages() {
   // Preload images
   const preloadedImages = [];
   const img = new Image();
+  img.src = `img/cat-yes.jpg`;
+  preloadedImages.push(img);
   for (let i = 1; i <= MAX_IMAGES; i++) {
     
     img.src = `img/cat-${i}.jpg`;
     preloadedImages.push(img);
   }
-  img.src = `img/cat-yes.jpg`;
-  preloadedImages.push(img);
+  
 }
 
 yesButton.addEventListener("click", handleYesClick);
